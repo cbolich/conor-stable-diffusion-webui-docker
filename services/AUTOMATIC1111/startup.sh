3557a346e76c1f446984ba6617bf8d924f10df2c
cbolich <conor.bolich@gmail.com>
2023-05-25 13:43:35 -0700

    extensions fixes

--- services/AUTOMATIC1111/startup.sh
+++ services/AUTOMATIC1111/startup.sh
@@ -7,12 +7,27 @@ done
 
 pip install -q --force-reinstall opencv-python 'transformers>=4.24'
 
-apt-get update && apt-get install -y fontconfig
+apt-get update && apt-get install -y fontconfig build-essential 
 
 pip install trimesh
 pip install moviepy
 pip install tqdm
 pip install transforms3d==4.0.1
+pip install networkx==2.3
+pip install cynetworkx
+pip install scikit-image
 pip install rembg
 pip install imageio-ffmpeg
 pip install vispy
+pip install svglib
+pip install rich
+pip install numexpr
+pip install matplotlib
+pip install pandas
+pip install av
+pip install pims
+pip install imageio_ffmpeg
+pip install gdown
+pip install mediapipe
+pip install fvcore
+pip install color-matcher
